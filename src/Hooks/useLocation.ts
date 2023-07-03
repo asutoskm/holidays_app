51b918b8e6b9295c29ad855f95bc0ca81a341b9f
mrakm <32322434+mrakm@users.noreply.github.com>
2023-07-03 12:15:32 +0530

    first commit

---
+++ src/Hooks/useLocation.ts
@@ -0,0 +1,82 @@
+import { useState, useEffect } from 'react';
+
+type Location = {
+  latitude: number | null;
+  longitude: number | null;
+  country: string | null;
+  error: string | null;
+};
+
+const useLocation = (): {country:string | null, isLoading:boolean} => {
+  const [location, setLocation] = useState<Location>({
+    latitude: null,
+    longitude: null,
+    country: null,
+    error: null,
+  });
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchCountry = async (latitude: number, longitude: number) => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(
+          `http://api.geonames.org/countryCodeJSON?lat=${latitude}&lng=${longitude}&username=komkom`
+        );
+        if (response.ok) {
+          const data = await response.json();
+          setLocation((prevLocation) => ({
+            ...prevLocation,
+            country: data.countryName,
+            error: null,
+          }));
+        } else {
+          throw new Error('Failed to fetch country');
+        }
+      } catch (error) {
+        setLocation((prevLocation) => ({
+          ...prevLocation,
+          error: 'someting went wrong',
+        }));
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    const getLocation = () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            setLocation((prevLocation) => ({
+              ...prevLocation,
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+              error: null,
+            }));
+
+            fetchCountry(position.coords.latitude, position.coords.longitude);
+          },
+          (error) => {
+            setLocation((prevLocation) => ({
+              ...prevLocation,
+              error: error.message,
+            }));
+            setIsLoading(false);
+          }
+        );
+      } else {
+        setLocation((prevLocation) => ({
+          ...prevLocation,
+          error: 'Geolocation is not supported',
+        }));
+        setIsLoading(false);
+      }
+    };
+
+    getLocation();
+  }, []);
+
+  return {country:location.country, isLoading};
+};
+
+export default useLocation;
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import useLocation from '@/Hooks/useLocation';
+import * as React from 'react';
+
+export interface IAppProps {
+}
+
+export default function App (props: IAppProps) {
+  const { country, isLoading } = useLocation();
+  return (
+    <div className='bg-slate-700 flex flex-col gap-5'>
+     <div>country {country}</div>
+     <div> isLoading {isLoading?'true':'false'}</div>
+    </div>
+  );
+}
